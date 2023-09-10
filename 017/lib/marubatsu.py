41b60317069a462b5fe9945d6a80061d20b6ee77
ysgeso <sigesada@hosei.ac.jp>
2023-09-10 23:09:11 +0900

    fin

---
+++ 017/lib/marubatsu.py
@@ -0,0 +1,8 @@
+def initialize_board():
+    return [[" "] * 3 for y in range(3)]
+
+def set_mark(board, x, y, mark):
+    if board[x][y] == " ":
+        board[x][y] = mark
+    else:
+        print("(", x, ",", y, ") のマスにはマークが配置済です")
