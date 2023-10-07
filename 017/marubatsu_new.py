a3d01830cf4b2e6b5513fe3eee7288f8ebd8e311
ysgeso <sigesada@hosei.ac.jp>
2023-10-07 20:19:33 +0900

    fin

--- 017/marubatsu_new.py
+++ 017/marubatsu_new.py
@@ -1,27 +1,27 @@
 def initialize_board() -> list[list[str]]:
-    """ 初期化されたゲーム盤のデータを返す関数.
+    """ 初期化されたゲーム盤のデータを返す.
 
     Returns:
-        list[list[str]]: 初期化されたゲーム盤を表す 2 次元配列の list.
+        初期化されたゲーム盤を表す 2 次元配列の list.
         全ての要素に半角の空白文字が代入されている.
     """
 
     return [[" "] * 3 for y in range(3)]
 
 def set_mark(board: list[list[str]], x: int, y: int , mark: str):
-    """ ゲーム盤の指定したマスに指定したマークを配置する関数.
+    """ ゲーム盤の指定したマスに指定したマークを配置する.
 
     (x, y) のマスに mark で指定したマークを配置する.
     (x, y) のマスに既にマークが配置済の場合は、メッセージを表示する.
 
     Args:
-        board (list[list[str]]):
+        board:
             マークを配置する、ゲーム盤を表す 2 次元配列の list
-        x (int):
+        x:
             マークを配置するマスの x 座標
-        y (int):
+        y:
             マークを配置するマスの y 座標
-        mark (str):
+        mark:
             配置するマークを表す文字列
     """
 
