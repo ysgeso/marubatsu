fa4db2fcf67d51d111dbc5e78556894926dea74e
ysgeso <sigesada@hosei.ac.jp>
2023-10-09 18:25:57 +0900

    fin

--- 017/lib/marubatsu.py
+++ 017/lib/marubatsu.py
@@ -1,5 +1,5 @@
 def initialize_board():
-    return [[" "] * 3 for y in range(3)]
+    return [[" "] * 3 for x in range(3)]
 
 def set_mark(board, x, y, mark):
     if board[x][y] == " ":
--- 018/marubatsu.py
+++ 018/marubatsu.py
@@ -6,7 +6,7 @@ def initialize_board() -> list[list[str]]:
         全ての要素に半角の空白文字が代入されている.
     """
 
-    return [[" "] * 3 for y in range(3)]
+    return [[" "] * 3 for x in range(3)]
 
 def place_mark(board: list[list[str]], x: int, y: int , mark: str):
     """ ゲーム盤の指定したマスに指定したマークを配置する.
