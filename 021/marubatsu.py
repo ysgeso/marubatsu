33fff80be9f258b0c741a8b741204b8bca5c2761
ysgeso <sigesada@hosei.ac.jp>
2023-10-22 19:47:37 +0900

    fin

--- 021/marubatsu.py
+++ 021/marubatsu.py
@@ -2,44 +2,22 @@ class Marubatsu:
     """ 〇×ゲーム.
 
     Attributes:
-        クラス属性
-        EMPTY (str):
-            空のマスを表す文字列
-        CIRCLE (str):
-            〇 のマークを表す文字列
-        CROSS (str):
-            × のマークを表す文字列
-        
-        インスタンス属性
-        BOARD_SIZE (int):
-            ゲーム盤の縦横のサイズを表す数値
         board (list[list[str]):
             ゲーム盤を表す 2 次元配列の list
             2 つのインデックスは、順に x 座標、y 座標を表す
             空白のマスは " "、〇 のマスは "o"、× のマスは "x" を代入する
     """
 
-    EMPTY = "."
-    CIRCLE = "o"
-    CROSS = "x"
-    
-    def __init__(self, board_size: int=3):
-        """ イニシャライザ 
-        
-        Args:
-            board_size:
-                ゲーム盤の縦横のサイズ。デフォルト値は 3
-        """
+    def __init__(self):
+        """ イニシャライザ """
 
-        # ゲーム盤の縦横のサイズ
-        self.BOARD_SIZE = board_size
         # ゲーム盤の初期化を行うメソッドを呼び出す
         self.initialize_board()
 
     def initialize_board(self):
         """ ゲーム盤のデータの初期化. """
 
-        self.board = [[Marubatsu.EMPTY] * self.BOARD_SIZE for y in range(self.BOARD_SIZE)]
+        self.board = [["."] * 3 for y in range(3)]
 
     def place_mark(self, x: int, y: int, mark: str):
         """ ゲーム盤の指定したマスに指定したマークを配置する.
@@ -56,7 +34,7 @@ class Marubatsu:
                 配置するマークを表す文字列
         """
 
-        if self.board[x][y] == Marubatsu.EMPTY:
+        if self.board[x][y] == ".":
             self.board[x][y] = mark
         else:
             print("(", x, ",", y, ") のマスにはマークが配置済です")
@@ -65,9 +43,9 @@ class Marubatsu:
         """ ゲーム盤の表示. """
 
         # 各行に対する繰り返しの処理を行う
-        for y in range(self.BOARD_SIZE):
+        for y in range(3):
             # y 行の各マスに対する繰り返しの処理を行う
-            for x in range(self.BOARD_SIZE):
+            for x in range(3):
                 # (x, y) のマスの要素を改行せずに表示する
                 print(self.board[x][y], end="")
             # x 列の表示が終わったので、改行する
