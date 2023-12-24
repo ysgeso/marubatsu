b79a02bb2085962276e7ebadf54dfcceecf4ea89
ysgeso <sigesada@hosei.ac.jp>
2023-12-24 18:25:28 +0900

    fin

---
+++ 039/marubatsu_new.py
@@ -0,0 +1,285 @@
+# 3.7 ～ 3.9 の Python のバージョンでエラーが発生しないようにするためのインポート
+from __future__ import annotations
+
+class Marubatsu:
+    """ 〇×ゲーム.
+
+    Attributes:
+        クラス属性
+        EMPTY (str):
+            空のマスを表す文字列
+        CIRCLE (str):
+            〇 のマークを表す文字列
+        CROSS (str):
+            × のマークを表す文字列
+        DRAW (str):
+            引き分けを表す文字列
+        PLAYING (str):
+            決着がついていないことを表す文字列
+        
+        インスタンス属性
+        BOARD_SIZE (int):
+            ゲーム盤の縦横のサイズを表す数値
+        board (list[list[str]):
+            ゲーム盤を表す 2 次元配列の list
+            2 つのインデックスは、順に x 座標、y 座標を表す
+            空白のマスは " "、〇 のマスは "o"、× のマスは "x" を代入する
+        move_count (int):
+            着手を行った回数
+        status (str):
+            ゲームの状態
+            CIRCLE、CROSS、DRAW、PLAYING のいずれかの状態が代入される
+        last_move (tuple(int, int)):
+            直前の着手の座標
+    """
+
+    EMPTY = "."
+    CIRCLE = "o"
+    CROSS = "x"
+    DRAW = "draw"
+    PLAYING = "playing"
+    
+    def __init__(self, board_size: int=3):
+        """ イニシャライザ 
+        
+        Args:
+            board_size:
+                ゲーム盤の縦横のサイズ。デフォルト値は 3
+        """
+
+        # ゲーム盤の縦横のサイズ
+        self.BOARD_SIZE = board_size
+        # 〇×ゲーム盤を再起動するメソッドを呼び出す
+        self.restart()
+
+    def initialize_board(self):
+        """ ゲーム盤のデータの初期化. """
+
+        self.board = [[Marubatsu.EMPTY] * self.BOARD_SIZE for y in range(self.BOARD_SIZE)]
+
+    def place_mark(self, x: int, y: int, mark: str) -> bool:
+        """ ゲーム盤の指定したマスに指定したマークを配置する.
+
+        (x, y) のマスに mark で指定したマークを配置する.
+        (x, y) のマスがゲーム盤の範囲外の場合は、メッセージを表示する.
+        (x, y) のマスに既にマークが配置済の場合は、メッセージを表示する.
+        返り値として配置できたかどうかを表す論理型のデータを返す.
+
+        Args:
+            x:
+                マークを配置するマスの x 座標
+            y:
+                マークを配置するマスの y 座標
+            mark:
+                配置するマークを表す文字列
+                
+        Returns:
+            マークを配置できた場合は True、配置できなかった場合は False
+        """
+
+        if 0 <= x < self.BOARD_SIZE and 0 <= y < self.BOARD_SIZE:
+            if self.board[x][y] == Marubatsu.EMPTY:
+                self.board[x][y] = mark
+                return True
+            else:
+                print("(", x, ",", y, ") のマスにはマークが配置済です")
+                return False
+        else:
+            print("(", x, ",", y, ") はゲーム盤の範囲外の座標です")
+            return False   
+            
+    def restart(self):
+        """ 〇×ゲームを再起動する. """
+
+        self.initialize_board()
+        self.turn = Marubatsu.CIRCLE     
+        self.move_count = 0
+        self.status = Marubatsu.PLAYING
+        self.last_move = -1, -1          
+
+    def move(self, x: int, y: int):
+        """ ゲーム盤の指定したマスに着手する.
+
+        Args:
+            x:
+                マークを配置するマスの x 座標
+            y:
+                マークを配置するマスの y 座標
+        """
+
+        if self.place_mark(x, y, self.turn):
+            self.turn = Marubatsu.CROSS if self.turn == Marubatsu.CIRCLE else Marubatsu.CIRCLE  
+            self.move_count += 1
+            self.status = self.judge()
+            self.last_move = x, y
+                    
+    def judge(self) -> str:
+        """ 勝敗判定.
+        
+        Returns:
+            以下のいずれかの値を返す
+            Marubatsu.CIRCLE:  〇 の勝利
+            Marubatsu.CROSS:   × の勝利
+            Marubatsu.DRAW:    引き分け
+            Marubatsu.PLAYING: 決着がついていない
+        """
+        
+        # 〇 の勝利の判定
+        if self.is_winner(Marubatsu.CIRCLE):
+            return Marubatsu.CIRCLE
+        # × の勝利の判定
+        elif self.is_winner(Marubatsu.CROSS):
+            return Marubatsu.CROSS   
+        # 引き分けの判定
+        elif self.is_full():
+            return Marubatsu.DRAW
+        # 上記のどれでもなければ決着がついていない
+        else:
+            return Marubatsu.PLAYING            
+            
+    def __str__(self):
+        # ゲームの決着がついていない場合は、手番を表示する
+        if self.status == Marubatsu.PLAYING:
+            text = "Turn " + self.turn + "\n"
+        # 決着がついていれば勝者を表示する
+        else:
+            text = "winner " + self.status + "\n"
+        for y in range(self.BOARD_SIZE):
+            for x in range(self.BOARD_SIZE):
+                lastx, lasty = self.last_move
+                if x == lastx and y == lasty:
+                    text += self.board[x][y].upper()
+                else:
+                    text += self.board[x][y]
+            text += "\n"
+        return text
+    
+    def is_winner(self, player:str) -> bool:
+        """player のプレイヤーの勝利判定.
+
+        Args:
+            player: 
+                判定するプレイヤーを表す文字列
+                Marubatsu.CIRCLE または Marubatsu.CROSS を指定する
+
+        Returns:
+            player のプレイヤーが勝利している場合は True
+            そうでない場合は False
+        """
+        
+        # 横方向と縦方向の判定
+        for i in range(self.BOARD_SIZE):
+            if self.is_same(player, coord=[0, i], dx=1, dy=0) or \
+            self.is_same(player, coord=[i, 0], dx=0, dy=1):
+                return True
+        # 左上から右下方向の判定
+        if self.is_same(player, coord=[0, 0], dx=1, dy=1):
+            return True
+        # 右上から左下方向の判定
+        if self.is_same(player, coord=[2, 0], dx=-1, dy=1):
+            return True
+
+        # どの一直線上にも配置されていない場合は、player は勝利していないので False を返す
+        return False
+   
+    def is_same(self, mark:str, coord:list[int], dx:int, dy:int) -> bool:
+        """指定したマスにマークが配置されているかの判定.
+        
+        coord のマスから、dx, dy 方向に存在するマスに、
+        mark のマークが配置されているかどうかを判定する
+
+        Args:
+            mark: 判定するマークを表す文字列
+                Marubatsu.CIRCLE または Marubatsu.CROSS を指定する
+            coord:
+                最初のマスの xy 座標を表す list
+            dx:
+                次のマスの x 座標の差分
+            dy:
+                次のマスの y 座標の差分
+
+        Returns:
+            すべてのマスに mark のマークが配置されている場合は True
+            そうでない場合は False
+        """
+        
+        x, y = coord
+        text_list = [self.board[x + i * dx][y + i * dy] 
+                     for i in range(self.BOARD_SIZE)]
+        line_text = "".join(text_list)
+        return line_text == mark * self.BOARD_SIZE
+
+    def is_full(self) -> bool:
+        """すべてのマスが埋まっているかどうかの判定.        
+
+        Returns:
+            すべてのマスが埋まっている場合は True
+            そうでない場合は False
+        """
+        
+        return self.move_count == self.BOARD_SIZE ** 2
+
+    def play(self, ai:list):
+        """〇×ゲームをプレイする.
+        
+        仮引数 ai でそれぞれの手番の着手を誰が担当するかを指定する
+        ゲーム盤が表示されるので、テキストボックスに着手を行う座標を入力する
+        座標は x,y の形式で入力する
+        
+        Args:
+            ai: それぞれの手番を誰が担当するかを指定する list
+                0 番の要素が 〇 の手番、1 番の要素が × の手番を表す
+                要素に None が代入されている場合は人間が担当することを表す
+                要素に関数が代入されている場合は、その関数によって AI が着手を行う
+                
+                AI の処理を行う関数の仮引数は Marubatsu クラスのインスタンスで、
+                着手を行う座標を表す (x, y) の形式の tuple を返すものとする
+        """
+        
+        # 〇×ゲームを再起動する
+        self.restart()
+        # ゲームの決着がついていない間繰り返す
+        while self.status == Marubatsu.PLAYING:
+            # ゲーム盤の表示
+            print(self)
+            # 現在の手番を表す ai のインデックスを計算する
+            index = 0 if self.turn == Marubatsu.CIRCLE else 1
+            # ai が着手を行うかどうかを判定する
+            if ai[index] is not None:
+                x, y = ai[index](self)
+            else:
+                # キーボードからの座標の入力
+                coord = input("x,y の形式で座標を入力して下さい。exit を入力すると終了します")
+                # "exit" が入力されていればメッセージを表示して関数を終了する
+                if coord == "exit":
+                    print("ゲームを終了します")
+                    return       
+                # x 座標と y 座標を要素として持つ list を計算する
+                xylist = coord.split(",")
+                # xylist の要素の数が 2 ではない場合
+                if len(xylist) != 2:
+                    # エラーメッセージを表示する
+                    print("x, y の形式ではありません")
+                    # 残りの while 文のブロックを実行せずに、次の繰り返し処理を行う
+                    continue
+                x, y = xylist
+            # (x, y) に着手を行う
+            try:
+                self.move(int(x), int(y))
+            except:
+                print("整数の座標を入力して下さい")
+
+        # 決着がついたので、ゲーム盤を表示する
+        print(self)
+
+    def calc_legal_moves(self) -> list[tuple[int, int]]:
+        """合法手の一覧を表す list を計算する.
+
+        Returns:
+            合法手の一覧を要素として持つ list
+            合法手は座標を表す (x, y) の形式の tuple
+        """
+        legal_moves = [(x, y) for y in range(self.BOARD_SIZE) 
+                            for x in range(self.BOARD_SIZE)
+                            if self.board[x][y] == Marubatsu.EMPTY]
+        return legal_moves
