18c52f028d80bcb1d2e74acea8345d35a4d5aed3
ysgeso <sigesada@hosei.ac.jp>
2024-06-16 23:23:48 +0900

    fin

--- 090/tree_new.py
+++ 090/tree_new.py
@@ -8,13 +8,15 @@ class Node:
     Attributes:
         mb:
             このノードの局面を表す Marubatsu クラスのインスタンス
+        depth:
+            このノードのゲーム木内での深さ
         height:
             このノードを描画した際の高さ
         childlen (list[Node]):
             子ノードの一覧を表す list
     """
     
-    def __init__(self, mb):
+    def __init__(self, mb:Marubatsu, depth:int=0):
         """ イニシャライザ.
         
         Args:
@@ -23,6 +25,7 @@ class Node:
         """
         
         self.mb = mb
+        self.depth = depth
         self.children = []
         
     def insert(self, node):
@@ -42,8 +45,8 @@ class Node:
         for x, y in self.mb.calc_legal_moves():
             childmb = deepcopy(self.mb)
             childmb.move(x, y)
-            self.insert(Node(childmb))
-
+            self.insert(Node(childmb, self.depth + 1))
+        
     def calc_height(self):
         """ノードを描画した際の高さを計算する."""
         
@@ -94,7 +97,6 @@ class Node:
         if len(self.children) > 0:   
             plt.plot([dx + 3.5, dx + 4, dx + 4], [dy + 1.5, dy + 1.5, dy + 1.5 + edgeheight], c="k", lw=lw)
             for childnode in self.children:
-                Marubatsu_GUI.draw_board(ax, childnode.mb, dx=dx+5, dy=dy, lw=lw)
                 plt.plot([dx + 4 , dx + 4.5], [dy + 1.5, dy + 1.5], c="k", lw=lw)
                 dy += childnode.height
 
@@ -104,6 +106,10 @@ class Mbtree:
     Attributes:
         root(Node):
             ルートノード
+        nodelist_by_depth(list[list[Node]]):
+            各深さのノードの list を記録する list
+        nodenum:
+            ゲーム木のノードの総数
     """
     
     def __init__(self):
@@ -113,53 +119,86 @@ class Mbtree:
     def create_tree_by_bf(self):
         """幅優先アルゴリズムによるゲーム木の作成."""
         
-        # 深さ 1 のノードの作成
-        self.root.calc_children()
-        # 深さ 2 のノードの作成
-        for childnode in self.root.children:
-            childnode.calc_children()
-            # 深さ 3 のノードの作成
-            for grandchildnode in childnode.children:
-                grandchildnode.calc_children()
-                # 深さ 4 のノードの作成
-                for greatgrandchildnode in grandchildnode.children:
-                    greatgrandchildnode.calc_children()
-                    
-    def calc_node_height(self):
-        """ゲーム木の各ノードの高さを計算する."""
+        # 深さ 0 のノードを、子ノードを作成するノードのリストに登録する
+        nodelist = [self.root]
+        depth = 0
+        # 各深さのノードのリストを記録する変数を初期化する
+        self.nodelist_by_depth = [ nodelist ]
         
-        for childnode in self.root.children:
-            # 深さ 2 のノードの高さの計算
-            for grandchildnode in childnode.children:
-                grandchildnode.calc_height()
-            # 深さ 1 のノードの高さの計算
-            childnode.calc_height()
-        # 深さ 0 のノードの高さの計算
-        self.root.calc_height()                    
+        # 深さ depth のノードのリストが空になるまで繰り返し処理を行う
+        while len(nodelist) > 0:
+            childnodelist = [] 
+            for node in nodelist:
+                if node.mb.status == Marubatsu.PLAYING:
+                    node.calc_children()
+                    childnodelist += node.children
+            self.nodelist_by_depth.append(childnodelist)
+            nodelist = childnodelist
+            depth += 1
+            print(f"{len(nodelist):>6} depth {depth} node created")
+            
+        self.nodenum = 0
+        for nodelist in self.nodelist_by_depth:
+            self.nodenum += len(nodelist)
+        print(f"total node num = {self.nodenum}")
                     
-    def draw_tree(self, size=0.25, lw=0.8):
-        """ゲーム木を描画する.
+    def calc_node_height(self, maxdepth:int):
+        """ゲーム木の各ノードの高さを計算する.
         
         Args:
+            maxdepth:
+                高さを計算するゲーム木の最大の深さ
+        """
+        
+        for depth in range(maxdepth, -1, -1):
+            for node in self.nodelist_by_depth[depth]:
+                if depth == maxdepth:
+                    node.height = 4
+                else:
+                    node.calc_height()                  
+                    
+    def draw_tree(self, startnode:Node=None, size:float=0.25, lw:float=0.8, maxdepth:int=2):
+        """ゲーム木の部分木を描画する.
+        
+        Args:
+            startnode:
+                描画を行う部分木のルートノード
             size:
                 描画する画像の倍率
             lw:
                 描画する図形の線の太さ
+            maxdepth:
+                描画を行うゲーム木の最大の深さ
         """
         
-        self.calc_node_height()   
-        width = 13
-        height = self.root.height
+        if startnode is None:
+            startnode = self.root
+        self.calc_node_height(maxdepth)
+        width = 5 * (maxdepth - startnode.depth + 1)
+        height = startnode.height
         fig, ax = plt.subplots(figsize=(width * size, height * size))
         ax.set_xlim(0, width)
         ax.set_ylim(0, height)   
         ax.invert_yaxis()
         ax.axis("off")        
-            
-        # 深さ 0 のノードの描画
-        self.root.draw_node(ax=ax, size=size, lw=lw)
-        dy = 0
-        # 深さ 1 のノードの描画
-        for childnode in self.root.children:
-            childnode.draw_node(ax=ax, size=size, lw=lw, dx=5, dy=dy)
-            dy += childnode.height
+        
+        nodelist = [startnode]
+        depth = startnode.depth
+        dx = 0
+        while len(nodelist) > 0 and depth <= maxdepth:        
+            dy = 0
+            childnodelist = []
+            for node in nodelist:
+                if node is None:
+                    dy += 4
+                    childnodelist.append(None)
+                else:
+                    node.draw_node(ax=ax, size=size, lw=lw, dx=dx, dy=dy)
+                    dy += node.height
+                    if len(node.children) > 0:  
+                        childnodelist += node.children
+                    else:
+                        childnodelist.append(None)
+            dx += 5
+            depth += 1
+            nodelist = childnodelist
