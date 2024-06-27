146738a02b5ab95826fa7411c72ef7a087839e16
ysgeso <sigesada@hosei.ac.jp>
2024-06-27 23:56:53 +0900

    fin

---
+++ 093/tree.py
@@ -0,0 +1,243 @@
+from copy import deepcopy
+import matplotlib.pyplot as plt
+from marubatsu import Marubatsu, Marubatsu_GUI
+
+class Node:
+    """ 〇×ゲームのゲーム木のノード.
+
+    Attributes:
+        mb:
+            このノードの局面を表す Marubatsu クラスのインスタンス
+        depth:
+            このノードのゲーム木内での深さ
+        height:
+            このノードを描画した際の高さ
+        childlen (list[Node]):
+            子ノードの一覧を表す list
+    """
+    
+    def __init__(self, mb:Marubatsu, parent=None, depth:int=0):
+        """ イニシャライザ.
+        
+        Args:
+            mb:
+                このノードの局面を表す Marubatsu クラスのインスタンス
+            parent:
+                親ノード。None の場合は親ノードが存在しないことを意味する
+            depth:
+                深さ
+        """
+
+        self.mb = mb
+        self.parent = parent
+        self.depth = depth
+        self.children = []
+        
+    def insert(self, node):
+        """ 子ノードを挿入する.
+        
+        Args:
+            node(Node):
+                挿入する Node クラスのインスタンス
+        """
+
+        self.children.append(node)
+        
+    def calc_children(self):
+        """子ノードを計算する"""
+        
+        self.children = []
+        for x, y in self.mb.calc_legal_moves():
+            childmb = deepcopy(self.mb)
+            childmb.move(x, y)
+            self.insert(Node(childmb, parent=self, depth=self.depth + 1))
+        
+    def calc_height(self):
+        """ノードを描画した際の高さを計算する."""
+        
+        if len(self.children) == 0:
+            self.height = 4
+        else:
+            self.height = 0
+            for childnode in self.children:
+                self.height += childnode.height
+
+    def draw_node(self, ax=None, maxdepth:int|None=None, size:float=0.25, lw:float=0.8, dx:float=0, dy:float=0):
+        """ノードと子ノードの関係を描画する.
+               
+        Args:
+            ax:
+                None の場合は、Figure を作成し、このノードと子ノードの関係のみを描画する
+                None でない場合は ax に、ノードの高さを考慮して描画する。そのため、
+                あらかじめノードの高さを計算しておく必要がある
+            maxdepth:
+                子ノードをしないノードの深さ
+            size:
+                描画する画像の倍率
+            lw:
+                描画する図形の線の太さ
+            dx:
+                描画する Axes 上の x 座標
+            dy:
+                描画する Axes 上の y 座標
+        """            
+        
+        width = 8
+        if ax is None:
+            height = len(self.children) * 4
+            fig, ax = plt.subplots(figsize=(width * size, height * size))
+            ax.set_xlim(0, width)
+            ax.set_ylim(0, height)   
+            ax.invert_yaxis()
+            ax.axis("off")
+            for childnode in self.children:
+                childnode.height = 4
+            self.height = height
+                
+        # 自分自身のノードを真ん中の位置になるように (dx, dy) からずらして描画する
+        y = dy + (self.height - 3) / 2
+        Marubatsu_GUI.draw_board(ax, self.mb, show_result=True, lw=lw, dx=dx, dy=y)
+        # 子ノードが存在する場合に、エッジの線と子ノードを描画する
+        if len(self.children) > 0:
+            if maxdepth != self.depth:   
+                plt.plot([dx + 3.5, dx + 4], [y + 1.5, y + 1.5], c="k", lw=lw)
+                prevy = None
+                for childnode in self.children:
+                    childnodey = dy + (childnode.height - 3) / 2
+                    if maxdepth is None:
+                        Marubatsu_GUI.draw_board(ax, childnode.mb, show_result=True, dx=dx+5, dy=childnodey, lw=lw)
+                    edgey = childnodey + 1.5
+                    plt.plot([dx + 4 , dx + 4.5], [edgey, edgey], c="k", lw=lw)
+                    if prevy is not None:
+                        plt.plot([dx + 4 , dx + 4], [prevy, edgey], c="k", lw=lw)
+                    prevy = edgey
+                    dy += childnode.height
+            else:
+                plt.plot([dx + 3.5, dx + 4.5], [y + 1.5, y + 1.5], c="k", lw=lw)
+
+class Mbtree:
+    """ 〇×ゲームのゲーム木のノード.
+
+    Attributes:
+        root(Node):
+            ルートノード
+        nodelist_by_depth(list[list[Node]]):
+            各深さのノードの list を記録する list
+        nodenum:
+            ゲーム木のノードの総数
+    """
+    
+    def __init__(self):
+        self.root = Node(Marubatsu())
+        self.create_tree_by_bf()
+        
+    def create_tree_by_bf(self):
+        """幅優先アルゴリズムによるゲーム木の作成."""
+        
+        # 深さ 0 のノードを、子ノードを作成するノードのリストに登録する
+        nodelist = [self.root]
+        depth = 0
+        # 各深さのノードのリストを記録する変数を初期化する
+        self.nodelist_by_depth = [ nodelist ]
+        
+        # 深さ depth のノードのリストが空になるまで繰り返し処理を行う
+        while len(nodelist) > 0:
+            childnodelist = [] 
+            for node in nodelist:
+                if node.mb.status == Marubatsu.PLAYING:
+                    node.calc_children()
+                    childnodelist += node.children
+            self.nodelist_by_depth.append(childnodelist)
+            nodelist = childnodelist
+            depth += 1
+            print(f"{len(nodelist):>6} depth {depth} node created")
+            
+        self.nodenum = 0
+        for nodelist in self.nodelist_by_depth:
+            self.nodenum += len(nodelist)
+        print(f"total node num = {self.nodenum}")
+                    
+    def calc_node_height(self, maxdepth:int):
+        """ゲーム木の各ノードの高さを計算する.
+        
+        Args:
+            maxdepth:
+                高さを計算するゲーム木の最大の深さ
+        """
+        
+        for depth in range(maxdepth, -1, -1):
+            for node in self.nodelist_by_depth[depth]:
+                if depth == maxdepth:
+                    node.height = 4
+                else:
+                    node.calc_height()                  
+                    
+    def draw_tree(self, startnode:Node|None=None, size:float=0.25, lw:float=0.8, maxdepth:int=2):
+        """ゲーム木の部分木を描画する.
+        
+        Args:
+            startnode:
+                描画を行う部分木のルートノード
+            size:
+                描画する画像の倍率
+            lw:
+                描画する図形の線の太さ
+            maxdepth:
+                描画を行うゲーム木の最大の深さ
+        """
+        
+        if startnode is None:
+            startnode = self.root
+        self.calc_node_height(maxdepth)
+        width = 5 * (maxdepth + 1)
+        height = startnode.height
+        parent = startnode.parent
+        if parent is not None:
+            height += (len(parent.children) - 1) * 4
+        parent.height = height
+        fig, ax = plt.subplots(figsize=(width * size, height * size))
+        ax.set_xlim(0, width)
+        ax.set_ylim(0, height)   
+        ax.invert_yaxis()
+        ax.axis("off")        
+        
+        nodelist = [startnode]
+        depth = startnode.depth
+        while len(nodelist) > 0 and depth <= maxdepth:        
+            dy = 0
+            if parent is not None:
+                dy = parent.children.index(startnode) * 4
+            childnodelist = []
+            for node in nodelist:
+                if node is None:
+                    dy += 4
+                    childnodelist.append(None)
+                else:
+                    dx = 5 * node.depth
+                    node.draw_node(ax=ax, maxdepth=maxdepth, size=size, lw=lw, dx=dx, dy=dy)
+                    dy += node.height
+                    if len(node.children) > 0:  
+                        childnodelist += node.children
+                    else:
+                        childnodelist.append(None)
+            depth += 1
+            nodelist = childnodelist
+            
+        if parent is not None:
+            dy = 0
+            for sibling in parent.children:
+                if sibling is not startnode:
+                    sibling.height = 4
+                    dx = 5 * sibling.depth
+                    sibling.draw_node(ax, maxdepth=sibling.depth, size=size, lw=lw, dx=dx, dy=dy)
+                dy += sibling.height
+            dx = 5 * parent.depth
+            parent.draw_node(ax, maxdepth=maxdepth, size=size, lw=lw, dx=dx, dy=0)
+        
+            node = parent
+            while node.parent is not None:
+                node = node.parent
+                node.height = height
+                dx = 5 * node.depth
+                node.draw_node(ax, maxdepth=node.depth, size=size, lw=lw, dx=dx, dy=0)
+        
